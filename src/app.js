e71a7796c7b44dd6935eccff86e4ac7250377ee9
camille <cerulean.dsgn@gmail.com>
2023-01-19 23:49:40 -0300

    post signup e post signin // inicio validações

--- src/app.js
+++ src/app.js
@@ -24,13 +24,57 @@ console.log("Erro no mongo.connect", err.message);
 
 
 // valida usuario
-// const userSchema = joi.object({
-//     name: joi.string().required()
-// })
+const userSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+    confirmPassword: joi.string().valid(joi.ref('password')).required()
+})
 // db = mongoClient.db("DIRETORIOOOO");
 // const talCollection = db.collection("COLLECTIONNNNN");
 
 
+
+app.post("/sign-up", async (req,res) => {
+    const { name, email, password, confirmPassword } = req.body //recebe parametro name a ser cadastrado
+    const userValidate = userSchema.validate({name, email, password, confirmPassword}) //validaçao 422
+    if (userValidate.error){
+        return res.sendStatus(422)
+    }
+
+    try {
+        // const userExist = await db.collection("user").findOne({name})
+        if (await db.collection('user').findOne({email})){
+            res.status(409).send("Este usuário já existe") 
+        }  //impedir cadastro de usuario ja existente
+
+       await db.collection('user').insertOne({name, email, password:bcrypt.hashSync(password,10)})
+        res.status(201).send("ok") //retornar status 201 pode retirar esse send ok 
+    } catch (err) {
+        console.log(err)
+        res.status(500).send("Deu algo errado no cadastro")
+    }
+})
+
+
+app.post("/sign-in", async (req,res) => {
+    const { email, password } = req.body;
+    const user = await db.collection('user').findOne({email})
+    if (!user) return res.status(401).send("E-mail não cadastrado!")
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) return res.status(401).send("Senha incorreta!")
+
+    const token = uuidV4();
+    const sessionInfo = {
+        userId: user._id,
+        token
+    };
+
+    await db.collection('session').insertOne(sessionInfo);
+    return res.send({token, name: user.name});
+    //necessario tratar erro no input
+});
 
 // ROTAS:
 
